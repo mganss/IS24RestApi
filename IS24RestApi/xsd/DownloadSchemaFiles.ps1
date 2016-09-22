e2e717d69f9896035ae324ff98b153f2581fb05e
Michael Ganss <michael.ganss@updatestar.com>
2016-09-22 16:24:49 +0200

    Add task runner file Update NuGet packages Update GIS Schema URL

--- IS24RestApi/xsd/DownloadSchemaFiles.ps1
+++ IS24RestApi/xsd/DownloadSchemaFiles.ps1
@@ -1,6 +1,6 @@
 ﻿foreach ($url in ((New-Object uri('http://rest.immobilienscout24.de/restapi/api/offer/v1.0/?_wadl&_schema')),
     (New-Object uri('http://rest.immobilienscout24.de/restapi/api/search/v1.0/?_wadl&_schema')),
-    (New-Object uri('http://rest.immobilienscout24.de/restapi/api/gis/v1.0/?_wadl&_schema'))))
+    (New-Object uri('http://rest.immobilienscout24.de/restapi/api/gis/v1.0/schema'))))
 {
     $base = $url.GetLeftPart([UriPartial]::Authority)
     $client = (New-Object System.Net.WebClient)
@@ -10,13 +10,13 @@
     $scriptpath = $MyInvocation.MyCommand.Path
     $dir = Split-Path $scriptpath
 
-    foreach ($match in $matches) 
+    foreach ($match in $matches)
     {
 	    $link = $base + $match.Groups[1].Value
 	    $ns = $match.Groups[2].Value
         $fn = $link | Select-String '[^/]+$' | %{ $_.Matches } | %{ $_.Value }
 
-        if ($fn -like "savedSearch*2*") 
+        if ($fn -like "savedSearch*2*")
         {
             continue;
         }
@@ -25,10 +25,10 @@
         $path += $ns + '\'
 
         if ($ns -eq "common" -and $fn -like "messages-*")
-        { 
+        {
             $path += 'includes\'
         }
-            
+
         md $path -Force
 
         $path += $fn
