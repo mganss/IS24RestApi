4851829de399428d97255f2fc5d63505c37fd803
Michael Ganss <michael@ganss.org>
2019-03-03 16:33:56 +0100

    Remove unneeded files

--- dist/package.cmd
+++
@@ -1,8 +0,0 @@
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ..\IS24RestApi.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
-set spec=".\IS24RestApi.generated.nuspec"
-set projs=..\IS24RestApi\IS24RestApi.csproj ..\IS24RestApi.Net40\IS24RestApi.Net40.csproj
-if exist %spec% del %spec%
-..\IS24RestApi.Pack\bin\Release\IS24RestApi.Pack.exe .\IS24RestApi.nuspec %spec% %projs%
-.\NuGet.exe update -self
-.\NuGet.exe pack -sym %spec%
-if exist %spec% del %spec%
